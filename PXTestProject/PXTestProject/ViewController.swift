abb92cf08c7b800f131b09ef3a4fb0dabfdaec25
augustocollerone <collerone.augusto@gmail.com>
2019-10-08 11:29:46 -0300

    Base project

---
+++ PXTestProject/PXTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PXTestProject
+//
+//  Created by AUGUSTO COLLERONE ALFONSO on 08/10/2019.
+//  Copyright © 2019 Mercado Pago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
