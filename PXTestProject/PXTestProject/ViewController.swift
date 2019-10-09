ca5e38eb9a8a268b47f06264f300dffb0fe240ff
augustocollerone <collerone.augusto@gmail.com>
2019-10-09 10:52:59 -0300

    Basic integration

--- PXTestProject/PXTestProject/ViewController.swift
+++ PXTestProject/PXTestProject/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import MercadoPagoSDK
+
+//#if PX_PRIVATE_POD
+//import MercadoPagoSDKV4
+//#else
+//import MercadoPagoSDK
+//#endif
+
+struct CheckoutConfigs {
+    let publicKey: String
+    let prefId: String
+    let lenguage: String
+    let color: UIColor
+    let paymentProcesorOn: Bool
+    let splitPaymentProcesorOn: Bool
+}
 
 class ViewController: UIViewController {
 
+    private var checkout: MercadoPagoCheckout?
+    var configs = CheckoutConfigs(publicKey: "TEST-4763b824-93d7-4ca2-a7f7-93539c3ee5bd",
+                                  prefId: "243966003-0812580b-6082-4104-9bce-1a4c48a5bc44",
+                                  lenguage: "es",
+                                  color: .orange,
+                                  paymentProcesorOn: false,
+                                  splitPaymentProcesorOn: false)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        runMercadoPagoCheckout(configs: configs)
+    }
+
+    private func runMercadoPagoCheckout(configs: CheckoutConfigs) {
+        // 1) Create Builder with your publicKey and preferenceId.
+        let builder = MercadoPagoCheckoutBuilder(publicKey: configs.publicKey, preferenceId: configs.prefId)
+
+        //Language customization
+        builder.setLanguage(configs.lenguage)
+
+        //Color customization
+        builder.setColor(checkoutColor: configs.color)
+
+        // 2) Create Checkout
+        checkout = MercadoPagoCheckout(builder: builder)
+
+        // 3) Start with your navigation controller.
+        if let myNavigationController = navigationController {
+            checkout?.start(navigationController: myNavigationController)
+        }
     }
 
 
